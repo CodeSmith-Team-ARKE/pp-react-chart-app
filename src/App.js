0af25da9abb1c2b459b34b81a310e5c6113bb0c5
Rebecca <32844316+RebeccaTurk@users.noreply.github.com>
2018-08-15 21:02:21 -0400

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,27 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+const echarts = require('echarts');
+import ReactEcharts from 'echarts-for-react';
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1 className="App-title">Welcome to React</h1>
+        </header>
+        <p className="App-intro">
+          To get started, edit <code>src/App.js</code> and save to reload.
+        </p>
+
+        <div>
+        Hi there I am a React App!
+        </div>
+
+      </div>
+    );
+  }
+}
+
+export default App;
